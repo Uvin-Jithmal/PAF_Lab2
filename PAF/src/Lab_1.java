c1a5d70403e8597fcb99e10d062ff6394b559004
Uvin-Jithmal <uvinjithmal@gmail.com>
2021-02-22 23:20:04 +0530

    Eclipse project commit

---
+++ PAF/src/Lab_1.java
@@ -0,0 +1,10 @@
+
+public class Lab_1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("We are redy for PAF");
+	}
+
+}
---
+++ PAF/src/Main.java
@@ -0,0 +1,9 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Add eclipse project to the Git hub");
+	}
+
+}
